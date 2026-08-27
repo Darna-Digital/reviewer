af17a2e1c9a7513ba9bad8566e2a1f3f01389006
Rūtenis Raila <rutenisraila@hotmail.com>
2026-08-27 20:45:06 +0300

    remove www and central-server packages

--- packages/www/prettier.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from "@byconvo/lint/prettier";
-
-/** @type {import("prettier").Config} */
-export default {
-  ...baseConfig,
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindStylesheet: "src/styles.css",
-  tailwindFunctions: ["cn"],
-};
